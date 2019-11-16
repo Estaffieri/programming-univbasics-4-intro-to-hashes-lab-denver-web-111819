b3d4a94a0680e5c27c267ba34c021a59ac90861c
Estelle S <estellelareinne@gmail.com>
2019-11-16 23:07:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,22 @@
 def new_hash
-  # return an empty hash
+  new_hash {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_hash ={
+    "shoes" => $200
+  }
 end
 
 def pioneer
+  pioneer={ :name => "Grace Hopper"}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_generator={ 
 end
 
 def my_hash_creator(key, value)
